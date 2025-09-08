910ad7fd70084c88030c914e01aec766ea170fd3
Leobstere <jerryzh163@gmail.com>
2025-09-08 14:10:22 -0400

    fixes

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,29 +6,28 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-        fizzingBuzzing();
-
-    }
-
-    private static void fizzingBuzzing() {
         int i = 0;
 
         while (i <= 100){
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("Fizz Buzz");
-            } else if (divisibleBy3) {
-                System.out.println("Fizz");
-            } else if (divisibleBy5) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
-
-            i++;
+            i = doBuzz(i);
         }
     }
+}
+
+private static int doBuzz(int i) {
+    boolean divisibleBy3 = i % 3 == 0;
+    boolean divisibleBy5 = i % 5 == 0;
+
+    if (divisibleBy3 && divisibleBy5) {
+        System.out.println("Fizz Buzz");
+    } else if (divisibleBy3) {
+        System.out.println("Fizz");
+    } else if (divisibleBy5) {
+        System.out.println("Buzz");
+    } else {
+        System.out.println(i);
     }
+
+    i++;
+    return i;
 }
