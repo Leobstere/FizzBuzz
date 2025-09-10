a8a5cb4ea28c859e69877d11f39b7d785584ad0f
Leobstere <jerryzh163@gmail.com>
2025-09-09 22:01:55 -0400

    Added override for multiples function to take no arguments.

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+    }
+    public static int reduce(int n){
+        int counter = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0){
+                n = n/2;
+                counter++;
+            } else {
+                n = n - 1;
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}
